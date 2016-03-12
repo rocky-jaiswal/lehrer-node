71a03115d93c4138a70c4be2119aa2701a540647
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2016-03-12 11:49:57 +0100

    minor edit.

--- config/routes.js
+++ config/routes.js
@@ -6,11 +6,11 @@ var GreetingsController = require('../app/controllers/greetings_controller'),
 
 var routes = {
   config: [
-    {method: 'GET',    path: '/api/hello',       config: {handler: GreetingsController.hello}},
-    {method: 'POST',   path: '/api/users',       config: {auth: false, handler: UsersController.create}},
-    {method: 'POST',   path: '/api/session',     config: {auth: false, handler: SessionsController.login}},
-    {method: 'POST',   path: '/api/phrases',     config: {handler: PhrasesController.create}},
-    {method: 'GET',    path: '/api/phrases',     config: {handler: PhrasesController.index}}
+    {method: 'GET',  path: '/api/hello',   config: {handler: GreetingsController.hello}},
+    {method: 'POST', path: '/api/users',   config: {auth: false, handler: UsersController.create}},
+    {method: 'POST', path: '/api/session', config: {auth: false, handler: SessionsController.login}},
+    {method: 'POST', path: '/api/phrases', config: {handler: PhrasesController.create}},
+    {method: 'GET',  path: '/api/phrases', config: {handler: PhrasesController.index}}
   ]
 };
 
